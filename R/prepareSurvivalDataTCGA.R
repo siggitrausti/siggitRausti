affd5885e3a158819e21597cf0c152cd526f7441
siggitrausti <stk13@hi.is>
2019-04-22 23:35:08 +0000

    22.04.19 added stuff

--- R/prepareSurvivalDataTCGA.R
+++ R/prepareSurvivalDataTCGA.R
@@ -17,13 +17,14 @@ prepareSurvivalDataTCGA <- function(dataset,genes,quantile_used,percentage_patie
     id_used_3 <- which(rownames(dataset) %in% genes$Group1)
   }
   survival_cl3_data <- dataset[id_used_3,]
-  CL3_z <- standardizeTCGA(survival_cl3_data)
+  #CL3_z <- standardizeTCGA(survival_cl3_data)
+  Cl3_z <- survival_cl3_data # 22.04.19 - this also changed...
   patients_assignment_vector <- rep(0,ncol(CL3_z))
   q_vals <- rep(0,nrow(CL3_z))
   for (j in 1:nrow(CL3_z)){
-    q_vals[j] <- quantile(as.numeric(CL3_z[j,3:ncol(CL3_z)]),quantile_used)
+    q_vals[j] <- quantile(as.numeric(CL3_z[j,1:ncol(CL3_z)]),quantile_used) # 22.04.19 - Changed to 1:ncol(..)
   }
-  for (i in 3:ncol(CL3_z)){
+  for (i in 1:ncol(CL3_z)){ # 22.04.19 - this also...
     q_comp = rep(0,nrow(CL3_z))
     for (j in 1:nrow(CL3_z)){
       q_comp[j] <- CL3_z[j,i] > q_vals[j]
