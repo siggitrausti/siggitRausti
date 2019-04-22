665eee0680bb03a908ac3c0e9230272adae1f610
siggitrausti <stk13@hi.is>
2019-04-22 23:42:43 +0000

    22.04.19 added more stuff

--- R/findBestSurvival.R
+++ R/findBestSurvival.R
@@ -28,7 +28,7 @@ findBestSurvival <- function(dataset,genelist,quantile_list,cor_data,print_plot=
   for (i in 1:length(q_vector)){
     patients_assignment_vector <- prepareSurvivalDataTCGA(dataset,genelist,q_vector[i],0.75)
     #print(length(which(patients_assignment_vector == 1)))
-    cor_data4 <- cbind(cor_data2,as.factor(patients_assignment_vector[3:length(patients_assignment_vector)]))
+    cor_data4 <- cbind(cor_data2,as.factor(patients_assignment_vector[1:length(patients_assignment_vector)])) # changed 22.04.19
     colnames(cor_data4)[ncol(cor_data4)] <- c('Cl3_high_low')
     res.cox2 <- coxph(Surv(survival, status) ~ Cl3_high_low, data = cor_data4)
     sum_coxph <- summary(res.cox2)
