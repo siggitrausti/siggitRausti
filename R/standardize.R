0fcb89e46465ba21f0ff314f26bae067488b43ec
siggitrausti <stk13@hi.is>
2019-05-03 13:49:11 +0000

    standardization avoiding NAN vals

--- R/standardize.R
+++ R/standardize.R
@@ -9,6 +9,9 @@ standardize <- function(data_frame){
   data_frame2 <- data_frame
   mean_vals <- apply(data_frame,1,function(x) mean(x,na.rm=T))
   sd_vals <- apply(data_frame,1,function(x) sd(x,na.rm=T))
+  if (any(sd_vals == 0)){
+    data_frame[which(sd_vals == 0),] = NULL
+  }
   for (i in 1:nrow(data_frame)){
     data_frame2[i,] <- lapply(data_frame[i,],function(x) ((x-mean_vals[i])/sd_vals[i]))
   }
