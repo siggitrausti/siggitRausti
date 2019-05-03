82f02c9072e7ba27f7147adc5d2151cb43423836
siggitrausti <stk13@hi.is>
2019-05-03 13:54:55 +0000

    Fixed the standardization vol2

--- R/standardize.R
+++ R/standardize.R
@@ -6,12 +6,12 @@
 #' @examples
 
 standardize <- function(data_frame){
-  data_frame2 <- data_frame
   mean_vals <- apply(data_frame,1,function(x) mean(x,na.rm=T))
   sd_vals <- apply(data_frame,1,function(x) sd(x,na.rm=T))
   if (any(sd_vals == 0)){
-    data_frame[which(sd_vals == 0),] = NULL
+    data_frame <- data_frame[-c(which(sd_vals == 0)),]
   }
+  data_frame2 <- data_frame
   for (i in 1:nrow(data_frame)){
     data_frame2[i,] <- lapply(data_frame[i,],function(x) ((x-mean_vals[i])/sd_vals[i]))
   }
