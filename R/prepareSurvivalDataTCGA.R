9ca96833037dcf8ca4bf2f8e778658e60e383525
siggitrausti <stk13@hi.is>
2019-05-03 10:35:09 +0000

    minor stuff

--- R/prepareSurvivalDataTCGA.R
+++ R/prepareSurvivalDataTCGA.R
@@ -16,29 +16,35 @@ prepareSurvivalDataTCGA <- function(dataset,genes,quantile_used,percentage_patie
     colnames(genes) = 'Group1'
     id_used_3 <- which(rownames(dataset) %in% genes$Group1)
   }
-  if (colnames(dataset)[1] == 'Hugo_Symbol'){
-    dataset <- dataset[,3:ncol(dataset)]
+  if (length(id_used_3) == 0){
+    patients_assignment_vector <- rep(1,ncol(dataset))
+    print(paste0("Gene(s) not found: ", genes))
+    return(patients_assignment_vector)
   } else {
-    dataset <- dataset
-  }
-  CL3_z <- standardize(dataset)
-  patients_assignment_vector <- rep(0,ncol(CL3_z))
-  q_vals <- rep(0,nrow(CL3_z))
-  for (j in 1:nrow(CL3_z)){
-    q_vals[j] <- quantile(as.numeric(CL3_z[j,1:ncol(CL3_z)]),quantile_used) # 22.04.19 - Changed to 1:ncol(..)
-  }
-  
-  for (i in 1:ncol(CL3_z)){ # 22.04.19 - this also...
-    q_comp = rep(0,nrow(CL3_z))
-    for (j in 1:nrow(CL3_z)){
-      q_comp[j] <- CL3_z[j,i] > q_vals[j]
-    }
-    q_comp <- as.numeric(q_comp)
-    if (length(which(q_comp == 1))/nrow(CL3_z) > percentage_patients){
-      patients_assignment_vector[i] = 1
+    if (colnames(dataset)[1] == 'Hugo_Symbol'){
+      dataset <- dataset[,3:ncol(dataset)]
     } else {
-      patients_assignment_vector[i] = 2
+      dataset <- dataset
     }
+    CL3_z <- standardize(dataset)
+    patients_assignment_vector <- rep(0,ncol(CL3_z))
+    q_vals <- rep(0,nrow(CL3_z))
+    for (j in 1:nrow(CL3_z)){
+      q_vals[j] <- quantile(as.numeric(CL3_z[j,1:ncol(CL3_z)]),quantile_used) # 22.04.19 - Changed to 1:ncol(..)
+    }
+    
+    for (i in 1:ncol(CL3_z)){ # 22.04.19 - this also...
+      q_comp = rep(0,nrow(CL3_z))
+      for (j in 1:nrow(CL3_z)){
+        q_comp[j] <- CL3_z[j,i] > q_vals[j]
+      }
+      q_comp <- as.numeric(q_comp)
+      if (length(which(q_comp == 1))/nrow(CL3_z) > percentage_patients){
+        patients_assignment_vector[i] = 1
+      } else {
+        patients_assignment_vector[i] = 2
+      }
+    }
+    return(patients_assignment_vector)
   }
-  return(patients_assignment_vector)
 }
