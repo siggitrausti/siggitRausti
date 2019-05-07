81073a1f951c3031713ba5f604c460e549c40e4a
siggitrausti <stk13@hi.is>
2019-05-07 11:37:59 +0000

    Changed standardization

--- R/standardize.R
+++ R/standardize.R
@@ -9,7 +9,10 @@ standardize <- function(data_frame){
   mean_vals <- apply(data_frame,1,function(x) mean(x,na.rm=T))
   sd_vals <- apply(data_frame,1,function(x) sd(x,na.rm=T))
   if (any(sd_vals == 0)){
-    data_frame <- data_frame[-c(which(sd_vals == 0)),]
+    id_remove <- which(sd_vals == 0)
+    data_frame <- data_frame[-c(id_remove),]
+    mean_vals <- mean_vals[-c(id_remove),]
+    sd_vals <- sd_vals[-c(id_remove),]
   }
   data_frame2 <- data_frame
   for (i in 1:nrow(data_frame)){
